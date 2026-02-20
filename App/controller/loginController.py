38ff7fa2df8475b10bb5eaf114824b1d4ecccaad
gus <gustavo.ggexpress@gmail.com>
2026-02-20 11:55:56 -0300

    loginController
    Fazendo as def's de validação e rotas de login.

--- App/controller/loginController.py
+++ App/controller/loginController.py
@@ -8,3 +8,16 @@ __currentUser = {
 
 def isLogged():
     return __currentUser['id']
+
+def logout():
+    __setCurrentUser(None)
+
+def __setCurrentUser(id):
+    __currentUser['id'] = id
+
+def validateLogin(email, password):
+    if email == "usuario" and password == "123":
+        __setCurrentUser(1)
+        return True
+    return False
+
